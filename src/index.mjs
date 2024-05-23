729f67bc344396a69ec3e47053e62b5ff709b563
Ajay Upadhyay <57ajay.u@gmail.com>
2024-05-23 23:59:07 +0530

    using sessions

--- src/index.mjs
+++ src/index.mjs
@@ -1,9 +1,15 @@
 import express from 'express';
 import routes from './routes/index.mjs';
 import cookieParser from 'cookie-parser';
-
+import session from 'express-session';
 
 const app = express(); 
+app.use(session({
+    secret: "Ajay",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {maxAge: 60000*60}
+}));
 app.use(cookieParser("Ajay"));
 const port = process.env.PORT || 3000;
 
@@ -11,8 +17,9 @@ app.use(express.json())
 app.use(routes);
 
 app.get('/', (req, res) => {
-    res.cookie("hello", "world", {maxAge: 100000, httpOnly: true, signed: true}).send("Hello World");
-
+    console.log(req.session, req.session.id);
+    req.session.visited = true;
+    res.cookie("hello", "world", {maxAge: 10000, httpOnly: true, signed: true}).send("Hello World");
 });
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
